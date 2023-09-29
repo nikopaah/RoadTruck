e8cecc17573cf04c586245a1c2eb1debf7f02867
nikopaah <nikopaah@gmail.com>
2023-09-29 13:58:52 -0300

    Road Truck project - First commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Vector3 offset = new(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
